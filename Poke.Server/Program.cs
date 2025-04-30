ade6253232b27dd45fe9ba071cb87334bae27875
Felipe Miosso <miosso.felipe@gmail.com>
2025-04-30 17:30:57 -0300

    initial commit

--- Poke.Server/Program.cs
+++ Poke.Server/Program.cs
@@ -7,23 +7,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+#if DEBUG
+builder.Services.AddDbContext<PokeContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+#else
 builder.Services.AddDbContext<PokeContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endif
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
     app.UseStaticFiles();
 
     using (var serviceScope = app.Services.CreateScope())
     using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<PokeContext>())
     {
-       //dbContext.Database.EnsureDeleted();
-       //dbContext.Database.EnsureCreated();
+       dbContext.Database.EnsureDeleted();
+       dbContext.Database.EnsureCreated();
     }
-//}
+}
 
 app.UseHttpsRedirection();
 
